61dc7c7600ef71ee80ce609b94438ceb9b0671ad
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-01-31 02:17:13 +0530

    Added custom button

--- src/js/emailPage.js
+++ src/js/emailPage.js
@@ -4,12 +4,54 @@ console.log(data);
 const Handlebars = require('handlebars');
 var nodemailer = require('nodemailer');
 
+var nameButton = function (context) {
+    var ui = $.summernote.ui;
+
+    var button = ui.button({
+        contents: '<i class="fa fa-child"/> Name',
+        tooltip: 'Inserts Name',
+        click: function () {
+            context.invoke('editor.insertText', '{{Name}}');
+        }
+    });
+    return button.render(); 
+}
+
+var occupationButton = function (context) {
+    var ui = $.summernote.ui;
+
+    var button = ui.button({
+        contents: '<i class="fa fa-child"/> Occupation',
+        tooltip: 'Inserts Occupation',
+        click: function () {
+            context.invoke('editor.insertText', '{{Occupation}}');
+        }
+    });
+    return button.render(); 
+}
+
 $(document).ready(function () {
     $('#summernote').summernote({
         height: 300,
         focus: true,
-        placeholder: "Enter body of email"
+        placeholder: "Enter body of email",
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['fontname', ['fontname']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['view', ['fullscreen', 'codeview', 'help']],
+            ['mybutton', ['name', 'occupation']]
+        ],
+        buttons: {
+            name: nameButton,
+            occupation: occupationButton,
+        }
     });
+
 });
 
 var transporter = nodemailer.createTransport({
@@ -30,20 +72,20 @@ document.querySelector('.se').addEventListener('click', function () {
     };
 
     data.forEach(element => {
-        console.log(element);
+
         mailOptions.to = element.EmailAddress;
         var current = element;
         var html = script(current);
         mailOptions.html = html;
-        transporter.sendMail(mailOptions, function (error, info) {
+
+        /*transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 console.log(error);
             } else {
                 console.log('Email sent: ' + info.response);
             }
-        });
+        });*/
     });
-    console.log(html);
 
 });
 
