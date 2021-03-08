a7197e7e63350db2e5c70e5a0d900e0c9b0a5818
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-03-09 03:01:18 +0530

    Improved Formatting

--- src/js/emailPage.js
+++ src/js/emailPage.js
@@ -3,83 +3,81 @@ console.log(data);
 
 const { dialog } = require("electron").remote;
 
-const Handlebars = require('handlebars');
-var nodemailer = require('nodemailer');
+const Handlebars = require("handlebars");
+var nodemailer = require("nodemailer");
 
 var nameButton = function (context) {
     var ui = $.summernote.ui;
 
     var button = ui.button({
         contents: '<i class="fa fa-child"/> Name',
-        tooltip: 'Inserts Name',
+        tooltip: "Inserts Name",
         click: function () {
-            context.invoke('editor.insertText', '{{Name}}');
-        }
+            context.invoke("editor.insertText", "{{Name}}");
+        },
     });
-    return button.render(); 
-}
+    return button.render();
+};
 
 var occupationButton = function (context) {
     var ui = $.summernote.ui;
 
     var button = ui.button({
         contents: '<i class="fa fa-child"/> Occupation',
-        tooltip: 'Inserts Occupation',
+        tooltip: "Inserts Occupation",
         click: function () {
-            context.invoke('editor.insertText', '{{Occupation}}');
-        }
+            context.invoke("editor.insertText", "{{Occupation}}");
+        },
     });
-    return button.render(); 
-}
+    return button.render();
+};
 
 $(document).ready(function () {
-    $('#summernote').summernote({
+    $("#summernote").summernote({
         height: 300,
         focus: true,
         placeholder: "Enter body of email",
         toolbar: [
-            ['style', ['style']],
-            ['font', ['bold', 'underline', 'clear']],
-            ['fontname', ['fontname']],
-            ['color', ['color']],
-            ['para', ['ul', 'ol', 'paragraph']],
-            ['table', ['table']],
-            ['insert', ['link', 'picture', 'video']],
-            ['view', ['fullscreen', 'codeview', 'help']],
-            ['mybutton', ['name', 'occupation']]
+            ["style", ["style"]],
+            ["font", ["bold", "underline", "clear"]],
+            ["fontname", ["fontname"]],
+            ["color", ["color"]],
+            ["para", ["ul", "ol", "paragraph"]],
+            ["table", ["table"]],
+            ["insert", ["link", "picture", "video"]],
+            ["view", ["fullscreen", "codeview", "help"]],
+            ["mybutton", ["name", "occupation"]],
         ],
         buttons: {
             name: nameButton,
             occupation: occupationButton,
-        }
+        },
     });
-
 });
 
-document.querySelector('#se').addEventListener('click', function () {
-
-    var email = document.getElementsByName('emailId')[0].value;
-    var password = document.getElementsByName('psw')[0].value;
-    var subject = document.getElementsByName('subject')[0].value;
+document.querySelector("#se").addEventListener("click", function () {
+    var email = document.getElementsByName("emailId")[0].value;
+    var password = document.getElementsByName("psw")[0].value;
+    var subject = document.getElementsByName("subject")[0].value;
 
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: "gmail",
         auth: {
             user: email,
             pass: password,
-        }
+        },
     });
 
-
-    var markupStr = $('#summernote').summernote('code');
+    var markupStr = $("#summernote").summernote("code");
     var script = Handlebars.compile(markupStr);
     var mailOptions = {
         from: email,
         subject: subject,
     };
 
-    var flag = 0;
-    data.forEach(element => {
+    var errorFlag = 0;
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
 
         mailOptions.to = element.EmailAddress;
         var current = element;
@@ -91,21 +89,21 @@ document.querySelector('#se').addEventListener('click', function () {
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 flag = 1;
-                dialog.showErrorBox('Error', error.message);
             } else {
-                console.log('Email sent: ' + info.response);
+                console.log("Email sent: " + info.response);
             }
 
-            if(flag == 0){
+            if (flag == 0 && errorFlag == 0) {
                 button.className = "btn btn-success";
-                button.innerText = "Success!"
-            }else{
+                button.innerText = "Success!";
+            } else {
                 button.className = "btn btn-danger";
-                button.innerText = "Error!!"
+                button.innerText = "Error!!";
+                if (errorFlag == 0) {
+                    dialog.showErrorBox("Error", error.message);
+                    errorFlag = 1;
+                }
             }
         });
-
-    });
-
+    }
 });
-
--- src/script.js
+++ src/script.js
@@ -5,123 +5,128 @@ var XLSX = require("xlsx");
 var _ = require("lodash");
 
 window.onload = function () {
+    var sampleData = [
+        [
+            "0",
+            1123,
+            "Stutend A",
+            "Occupation A",
+            "Sample Question",
+            "xyz@gmail.com",
+            "Organization A",
+        ],
+        [
+            "0",
+            456,
+            "Stutend B",
+            "Occupation B",
+            "Sample Question",
+            "abc@gmail.com",
+            "Organization B",
+        ],
+    ];
 
-  var sampleData = [
-    [
-      "0",
-      1123,
-      "Stutend A",
-      "Occupation A",
-      "Sample Question",
-      "xyz@gmail.com",
-      "Organization A",
-    ],
-    [
-      "0",
-      456,
-      "Stutend B",
-      "Occupation B",
-      "Sample Question",
-      "abc@gmail.com",
-      "Organization B",
-    ],
-  ];
+    var database;
 
-  var database;
+    $(document).ready(function () {
+        var table = $("#table_id").DataTable({
+            data: sampleData,
+            columnDefs: [
+                {
+                    targets: 0,
+                    checkboxes: true,
+                },
+            ],
+            select: {
+                style: "multi",
+            },
+            order: [[2, "asc"]],
+        });
 
-  $(document).ready(function () {
-    var table = $("#table_id").DataTable({
-      data: sampleData,
-      columnDefs: [
-        {
-          targets: 0,
-          checkboxes: true,
-        },
-      ],
-      select: {
-        style: "multi",
-      },
-      order: [[2, "asc"]],
+        $("#send-email").on("click", function (e) {
+            const filters = JSON.parse(
+                languages.getSelectedOptionsAsJson((includeDisabled = true))
+            );
+            var toSearch = filters.occupation.join("|");
+            table.columns(3).search(toSearch, true, false, true).draw();
+
+            var rows_selected = table.column(0).checkboxes.selected();
+            var form = [];
+
+            $.each(rows_selected, function (index, rowId) {
+                form.push(rowId);
+            });
+
+            //Get the data in presentable form
+            var filteredData = [];
+            form.forEach((index) => filteredData.push(database[index]));
+            console.log(filteredData);
+
+            localStorage.setItem("filteredData", JSON.stringify(filteredData));
+            ipcRenderer.send("main:add");
+        });
+
+        $("#filter").on("click", function () {
+            const filters = JSON.parse(
+                languages.getSelectedOptionsAsJson((includeDisabled = true))
+            );
+            var toSearch = filters.occupation.join("|");
+            table.columns(3).search(toSearch, true, false, true).draw();
+        });
     });
 
-    $("#send-email").on("click", function (e) {
-      const filters = JSON.parse(
-        languages.getSelectedOptionsAsJson((includeDisabled = true))
-      );
-      var toSearch = filters.occupation.join("|");
-      table.columns(3).search(toSearch, true, false, true).draw();
+    function renderTable(database) {
+        console.log(database);
 
-      var rows_selected = table.column(0).checkboxes.selected();
-      var form = [];
+        var datables = [];
+        var index = 0;
+        database.forEach(function (value) {
+            var beh = index.toString();
+            var currObj = [beh];
+            for (var key of Object.keys(value)) {
+                currObj.push(value[key]);
+            }
+            index++;
+            datables.push(currObj);
+        });
 
-      $.each(rows_selected, function (index, rowId) {
-        form.push(rowId);
-      });
+        $("#table_id").DataTable().clear();
+        datables.forEach(function (value) {
+            $("#table_id").DataTable().row.add(value);
+        });
+        $("#table_id").DataTable().draw();
+    }
 
-      //Get the data in presentable form
-      var filteredData = [];
-      form.forEach((index) => filteredData.push(database[index]));
-      console.log(filteredData);
-
-      localStorage.setItem("filteredData", JSON.stringify(filteredData));
-      ipcRenderer.send("main:add");
+    var languages = $("#occupations").filterMultiSelect({
+        placeholderText: "nothing selected",
+        filterText: "Filter",
+        selectAllText: "Select All",
+        caseSensitive: false,
+        allowEnablingAndDisabling: true,
     });
 
-    $("#filter").on("click", function () {
-      const filters = JSON.parse(
-        languages.getSelectedOptionsAsJson((includeDisabled = true))
-      );
-      var toSearch = filters.occupation.join("|");
-      table.columns(3).search(toSearch, true, false, true).draw();
-    });
-  });
-
-  function renderTable(database) {
-    console.log(database);
-
-    var datables = [];
-    var index = 0;
-    database.forEach(function (value) {
-      var beh = index.toString();
-      var currObj = [beh];
-      for (var key of Object.keys(value)) {
-        currObj.push(value[key]);
-      }
-      index++;
-      datables.push(currObj);
-    });
-
-    $("#table_id").DataTable().clear();
-    datables.forEach(function (value) {
-      $("#table_id").DataTable().row.add(value);
-    });
-    $("#table_id").DataTable().draw();
-  }
-
-  var languages = $("#occupations").filterMultiSelect({
-    placeholderText: "nothing selected",
-    filterText: "Filter",
-    selectAllText: "Select All",
-    caseSensitive: false,
-    allowEnablingAndDisabling: true,
-  });
-
-  document.getElementById("import-data").onclick = function loadExcel() {
-    dialog
-      .showOpenDialog({
-        properties: ["openFile"],
-        filters: [{ name: "All Files", extensions: ["xlsx"] }],
-      })
-      .then((result) => {
-        workbook = XLSX.readFile(result.filePaths[0]);
-        var sheet_name_list = workbook.SheetNames;
-        database = XLSX.utils
-          .sheet_to_json(workbook.Sheets[sheet_name_list[0]], { defval: "" })
-          .map((row) => _.mapKeys(row, (value, key) => key.trim().replace(/\s/g, "")));
-        renderTable(database);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    document.getElementById("import-data").onclick = function loadExcel() {
+        dialog
+            .showOpenDialog({
+                properties: ["openFile"],
+                filters: [{ name: "All Files", extensions: ["xlsx"] }],
+            })
+            .then((result) => {
+                workbook = XLSX.readFile(result.filePaths[0]);
+                var sheet_name_list = workbook.SheetNames;
+                database = XLSX.utils
+                    .sheet_to_json(workbook.Sheets[sheet_name_list[0]], {
+                        defval: "",
+                    })
+                    .map((row) =>
+                        _.mapKeys(row, (value, key) =>
+                            key.trim().replace(/\s/g, "")
+                        )
+                    );
+                renderTable(database);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 };
